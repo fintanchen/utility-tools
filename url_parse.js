bf34edc14c59c502482256937850abbf455c8a3a
yvwalker <yvwalker1024@gmail.com>
2018-12-13 22:40:36 +0800

    create a workspqce of ts

--- url_parse.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// @ts-check
-exports.__esModule = true;
-// import url from 'url';
-var os_1 = require("os");
-console.log(os_1["default"].arch());
--- url_parse.ts
+++
@@ -1,6 +0,0 @@
-// @ts-check
-
-// import url from 'url';
-import os from "os";
-
-console.log(os.arch())
