9084953d7640b7d2a3a08a09c1385ded434f6dd5
yvwalker <yvwalker1024@gmail.com>
2018-12-13 15:27:28 +0800

    ceeate url_parse

---
+++ url_parse.js
@@ -0,0 +1,6 @@
+"use strict";
+// @ts-check
+exports.__esModule = true;
+// import url from 'url';
+var os_1 = require("os");
+console.log(os_1["default"].arch());
---
+++ url_parse.ts
@@ -0,0 +1,6 @@
+// @ts-check
+
+// import url from 'url';
+import os from "os";
+
+console.log(os.arch())
