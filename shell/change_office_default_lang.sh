be8cec4079c78db90c05874f930acd7e3d6ffbbe
Fintan Chen <fintan@CMCC-EDU.mshome.net>
2019-03-26 11:01:56 +0800

    shell: add a script to change the default language of office to zh-CN

---
+++ shell/change_office_default_lang.sh
@@ -0,0 +1,5 @@
+defaults write com.microsoft.Excel AppleLanguages '("zh-CN")'
+defaults write com.microsoft.Word AppleLanguages '("zh-CN")'
+defaults write com.microsoft.Powerpoint AppleLanguages '("zh-CN")'
+defaults write com.microsoft.Outlook AppleLanguages '("zh-CN")'
+
