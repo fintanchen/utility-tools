28aafb55ed640be1706c6a455499e7d824d180d3
keyvchan <keyvchan@gmail.com>
2019-07-19 14:29:36 +0800

    reverse grep the auto-update package in brew. Renew the location of the golang package update script

--- shell/upgrade_all_pkgs.sh
+++ shell/upgrade_all_pkgs.sh
@@ -1,71 +1,70 @@
 #!/bin/bash
 
-function newface(){
-    sleep 1
-    clear
+function newface() {
+	sleep 1
+	clear
 }
 
 # for debian-based operate system.
-if  command -v apt-get > /dev/null 2>&1;then
-    apt update && apt dist-upgrade -y
+if command -v apt-get >/dev/null 2>&1; then
+	apt update && apt dist-upgrade -y
 else
-    echo "Don't have apt"
+	echo "Don't have apt"
 fi
 
 newface
 
 # for redhat-based operate system.
-if  command -v yum > /dev/null 2>&1;then
-    yum update -y
+if command -v yum >/dev/null 2>&1; then
+	yum update -y
 else
-    echo "Don't have yum"
+	echo "Don't have yum"
 fi
 
 newface
 
 # for macOS
-if  command -v brew > /dev/null 2>&1;then
-    # update source of brew core and cask
-    brew update
+if command -v brew >/dev/null 2>&1; then
+	# update source of brew core and cask
+	brew update
 
-    # update brew core
-    brew upgrade
+	# update brew core
+	brew upgrade
 
-    # update brew cask
-    brew cask upgrade `brew cask list`
+	# update brew cask
+	brew cask upgrade $(brew cask list | grep -v battle | grep -v steam | grep -v gramm)
 
-    # clean brew install and unused dirs
-    brew cleanup
+	# clean brew install and unused dirs
+	brew cleanup
 else
-    echo "Don't have brew"
+	echo "Don't have brew"
 fi
 
 newface
 
 # common.
-if  command -v yarn > /dev/null 2>&1;then
-    # yarn
-    yarn global upgrade
+if command -v yarn >/dev/null 2>&1; then
+	# yarn
+	yarn global upgrade
 
 else
-    echo "Don't have yarn"
+	echo "Don't have yarn"
 fi
 
 newface
 
-if  command -v rustup > /dev/null 2>&1;then
-    # rust
-    rustup update
+if command -v rustup >/dev/null 2>&1; then
+	# rust
+	rustup update
 else
-    echo "Don't have rustup"
+	echo "Don't have rustup"
 fi
 
 newface
 
-if  command -v go > /dev/null 2>&1;then
-    # go
-    go run $HOME/unSync/utility-tools/go/update_gopath_repo/upgrade_all_go_pkgs.go
+if command -v go >/dev/null 2>&1; then
+	# go
+	go run $HOME/unsync/github.com/keyvchan/utility-tools/go/update_gopath_repo/upgrade_all_go_pkgs.go
 else
-    echo "Don't have go"
+	echo "Don't have go"
 fi
-
